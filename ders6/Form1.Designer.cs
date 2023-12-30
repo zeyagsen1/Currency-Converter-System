41f47193778a185c483668fdaac82c510359498b
zeyagsen1 <ibrahimyesilagac0@gmail.com>
2023-12-30 18:21:18 +0300

    first commit

---
+++ ders6/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ders6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox1.Location = new System.Drawing.Point(455, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox2.Location = new System.Drawing.Point(455, 219);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "US DOLLAR (USD)",
+            "EURO(EUR)",
+            "POUND STERLING(GBP)",
+            "SWISS FRANK(CHF)",
+            "CANADIAN DOLLAR(CAD)",
+            "KUWAITI DINAR(KWD)",
+            "NORWEGIAN KRONE(NOK)",
+            "JAPENESE YEN(JPY)",
+            "BULGARIAN LEV(BGN)",
+            "RUSSIAN ROUBLE(RUB)",
+            "CHINESE RENMINBI(CNY)",
+            "SOUTH KOREAN WON(KRW)",
+            "AZERBAIJANI NEW MANAT(AZN)",
+            "UNITED ARAB EMIRATES DIRHAM(AED)",
+            "TURKISH LIRA(TL)"});
+            this.comboBox1.Location = new System.Drawing.Point(183, 160);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(227, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseClick);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "US DOLLAR (USD)",
+            "EURO(EUR)",
+            "POUND STERLING(GBP)",
+            "SWISS FRANK(CHF)",
+            "CANADIAN DOLLAR(CAD)",
+            "KUWAITI DINAR(KWD)",
+            "NORWEGIAN KRONE(NOK)",
+            "JAPENESE YEN(JPY)",
+            "BULGARIAN LEV(BGN)",
+            "RUSSIAN ROUBLE(RUB)",
+            "CHINESE RENMINBI(CNY)",
+            "SOUTH KOREAN WON(KRW)",
+            "AZERBAIJANI NEW MANAT(AZN)",
+            "UNITED ARAB EMIRATES DIRHAM(AED)",
+            "TURKISH LIRA(TL)"});
+            this.comboBox2.Location = new System.Drawing.Point(183, 219);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(227, 24);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox2_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(124, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(430, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Currency to Convert";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(459, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Convert Currency";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ders6/Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ders6
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        bool t1run = false;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox2.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 1;
+            timer1.Start();
+        }
+
+        
+        int counter;
+        public void updateData()
+        {
+            counter++;
+            label2.Text = DateTime.Now.ToString();
+           
+            if (t1run)
+            {
+                if (counter > 10000)
+                {
+                    convertUpCurrency();
+                    counter = 0;
+                }
+                label2.Text = DateTime.Now.ToString() ;
+
+
+            }
+
+            else {
+                if (counter > 10000)
+                {
+                    converDownCurrency();
+                    counter = 0;
+                }
+                
+                label2.Text = DateTime.Now.ToString();
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+        public void converDownCurrency()
+        {
+
+            try
+            {
+                if (textBox2.Text != "" && textBox2.Text != "0" && t1run == false)
+                {
+
+                    string t2 = comboBox1.Text;
+                    string t1 = comboBox2.Text;
+                    int startIndex = t1.IndexOf('(');
+                    int endIndex = t1.IndexOf(')');
+                    string newT1 = "";
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        newT1 += t1[i];
+
+
+                    }
+
+
+                    int startIndexx = t2.IndexOf('(');
+                    int endIndexx = t2.IndexOf(')');
+                    string newT2 = "";
+                    for (int i = startIndexx + 1; i < endIndexx; i++)
+                    {
+                        newT2 += t2[i];
+
+
+                    }
+
+                    if (newT1 == "TL" || newT2 == "TL")
+                    {
+
+
+                        if (newT1 != "TL")
+                        {
+                            //tl = true;
+
+                            XmlDocument xmlll = new XmlDocument();
+                            xmlll.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                            XmlNode roottt = xmlll.DocumentElement;
+                            XmlNode usdNodeee = roottt.SelectSingleNode($"//Currency[@Kod='{newT1}']");
+
+
+
+
+
+                            CultureInfo culturee = CultureInfo.InvariantCulture;
+                            XmlNode forexBuyingNodeee = usdNodeee.SelectSingleNode("ForexBuying");
+                            double valueee = double.Parse(forexBuyingNodeee.InnerText, culturee);
+                            double p = double.Parse(textBox2.Text);
+                            double resulttt = ((valueee)) * p;
+                            textBox1.Text = resulttt.ToString();
+
+                        }
+
+                        if (newT2 != "TL")
+                        {
+                            //tll = true;
+
+                            XmlDocument xmlll = new XmlDocument();
+                            xmlll.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                            XmlNode roottt = xmlll.DocumentElement;
+                            XmlNode usdNodeee = roottt.SelectSingleNode($"//Currency[@Kod='{newT2}']");
+
+
+
+
+
+                            CultureInfo culturee = CultureInfo.InvariantCulture;
+                            XmlNode forexBuyingNodeee = usdNodeee.SelectSingleNode("ForexBuying");
+                            double valueee = double.Parse(forexBuyingNodeee.InnerText, culturee);
+                            double p = double.Parse(textBox2.Text);
+                            double resulttt = 1/(valueee) * p;
+                            textBox1.Text = resulttt.ToString();
+
+                        }
+
+
+
+                    }
+                    else
+                    {
+
+                        XmlDocument xmll = new XmlDocument();
+                        xmll.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                        XmlNode roott = xmll.DocumentElement;
+                        XmlNode usdNodee = roott.SelectSingleNode($"//Currency[@Kod='{newT2}']");
+
+
+                        Console.WriteLine(newT2);
+
+
+                        CultureInfo culture = CultureInfo.InvariantCulture;
+                        XmlNode forexBuyingNodee = usdNodee.SelectSingleNode("ForexBuying");
+                        double valuee = double.Parse(forexBuyingNodee.InnerText, culture);
+
+
+
+
+
+
+
+
+                        XmlDocument xml = new XmlDocument();
+                        xml.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                        XmlNode root = xml.DocumentElement;
+                        XmlNode usdNode = root.SelectSingleNode($"//Currency[@Kod='{newT1}']");
+
+
+                        Console.WriteLine(newT1);
+
+
+
+
+                        XmlNode forexBuyingNode = usdNode.SelectSingleNode("ForexBuying");
+                        double value = double.Parse(forexBuyingNode.InnerText, culture);
+                        Console.WriteLine(forexBuyingNode.InnerText + "+asdsada");
+
+                        string k = forexBuyingNode.InnerText.Split(',')[0];
+
+                        Console.WriteLine(value + " ...." + valuee);
+
+                        double rate = value / valuee;
+                        Console.WriteLine(rate);
+
+                        double result = rate * Double.Parse(textBox2.Text);
+
+
+                        Console.WriteLine(result);
+                        string resultt = result.ToString();
+
+                        textBox1.Text = result.ToString();
+
+
+                    }
+                }
+            }
+
+            catch (Exception e) { 
+            
+            MessageBox.Show(e.Message); 
+            
+            }   
+
+
+        }
+        public void convertUpCurrency()
+        {
+
+            try
+            {
+                if (textBox1.Text != "" && textBox1.Text != "0" && t1run == true)
+                {
+                    string t1 = comboBox1.Text;
+                    string t2 = comboBox2.Text;
+                    int startIndex = t1.IndexOf('(');
+                    int endIndex = t1.IndexOf(')');
+                    string newT1 = "";
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        newT1 += t1[i];
+
+
+                    }
+
+
+
+
+                    int startIndexx = t2.IndexOf('(');
+                    int endIndexx = t2.IndexOf(')');
+                    string newT2 = "";
+                    for (int i = startIndexx + 1; i < endIndexx; i++)
+                    {
+                        newT2 += t2[i];
+
+
+                    }
+
+
+                    if (newT1 == "TL" || newT2 == "TL")
+                    {
+
+
+                        if (newT1 != "TL")
+                        {
+                            
+
+                            XmlDocument xmlll = new XmlDocument();
+                            xmlll.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                            XmlNode roottt = xmlll.DocumentElement;
+                            XmlNode usdNodeee = roottt.SelectSingleNode($"//Currency[@Kod='{newT1}']");
+
+
+
+
+
+                            CultureInfo culturee = CultureInfo.InvariantCulture;
+                            XmlNode forexBuyingNodeee = usdNodeee.SelectSingleNode("ForexBuying");
+                            double valueee = double.Parse(forexBuyingNodeee.InnerText, culturee);
+                            double p = double.Parse(textBox1.Text);
+                            double resulttt = (valueee) * p;
+                            textBox2.Text = resulttt.ToString();
+
+                        }
+
+                        if (newT2 != "TL")
+                        {
+
+                            XmlDocument xmlll = new XmlDocument();
+                            xmlll.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                            XmlNode roottt = xmlll.DocumentElement;
+                            XmlNode usdNodeee = roottt.SelectSingleNode($"//Currency[@Kod='{newT2}']");
+
+
+
+
+
+                            CultureInfo culturee = CultureInfo.InvariantCulture;
+                            XmlNode forexBuyingNodeee = usdNodeee.SelectSingleNode("ForexBuying");
+                            double valueee = double.Parse(forexBuyingNodeee.InnerText, culturee);
+                            double p = double.Parse(textBox1.Text);
+                            double resulttt = (1/(valueee)) * p;
+                            textBox2.Text = resulttt.ToString();
+
+                        }
+
+
+
+                    }
+
+                    else
+                    {
+                       
+
+                        CultureInfo culture = CultureInfo.InvariantCulture;
+
+
+                        XmlDocument xmll = new XmlDocument();
+                        xmll.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                        XmlNode roott = xmll.DocumentElement;
+
+                        XmlNode usdNodee = roott.SelectSingleNode($"//Currency[@Kod='{newT2}']");
+
+
+                        Console.WriteLine(newT2);
+
+
+
+                        XmlNode forexBuyingNodee = usdNodee.SelectSingleNode("ForexBuying");
+                        double valuee = Double.Parse(forexBuyingNodee.InnerText, culture);
+
+
+
+
+
+                        XmlDocument xml = new XmlDocument();
+                        xml.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+                        XmlNode root = xml.DocumentElement;
+                        XmlNode usdNode = root.SelectSingleNode($"//Currency[@Kod='{newT1}']");
+
+                        Console.WriteLine(newT1);
+
+
+
+
+                        XmlNode forexBuyingNode = usdNode.SelectSingleNode("ForexBuying");
+                        double value = Double.Parse(forexBuyingNode.InnerText, culture);
+
+
+                        double rate = value / valuee;
+
+
+                        double result = rate * Double.Parse(textBox1.Text);
+
+                        string resultt = result.ToString();
+                        Console.WriteLine(result);
+
+                        textBox2.Text = resultt;
+
+
+
+
+                    }
+                }
+            }
+            catch (Exception e) { 
+            
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+
+             convertUpCurrency();
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            converDownCurrency();
+           
+           
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            convertUpCurrency();
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+            converDownCurrency();
+        }
+
+       
+
+      
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            t1run = false;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            t1run = true;
+
+            
+
+
+        }
+
+        private void comboBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            t1run = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            updateData();
+            
+        }
+
+        private void comboBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            t1run = false;
+        }
+
+    }
+}
+
